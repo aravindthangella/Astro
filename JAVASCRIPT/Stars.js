42263c55b0d8e6921fed325f9eb816b8f5d3c285
aravindthangella <127821562+aravindthangella@users.noreply.github.com>
2023-03-28 20:24:13 +0530

    Add files via upload

---
+++ JAVASCRIPT/Stars.js
@@ -0,0 +1,48 @@
+var images = document.getElementsByClassName("a")
+for (i = 1; i <=images.length; i++)
+{
+    images[i-1].setAttribute("src","../ASSET/Images/stars/"+i+".jpg")
+    images[i-1].style.width="220px"
+    images[i-1].style.height="220px"
+}
+console.log(images)
+//this is for header
+var indval=0;
+var images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"]
+        function inc()
+        {
+            indval++;
+            if(indval>=images.length)
+            {
+                indval=0;
+            document.getElementById("header").style.backgroundImage="url(../ASSET/Images/stars/"+images[indval]+")";
+            }
+            else
+            {
+                document.getElementById("header").style.backgroundImage="url(../ASSET/Images/stars/"+images[indval]+")";
+            }
+        }
+        function dec()
+        {
+            indval--;
+            if(indval<0)
+            {
+                indval=9;
+            document.getElementById("header").style.backgroundImage="url(../ASSET/Images/stars/"+images[indval]+")";
+            }
+            else
+            {
+                document.getElementById("header").style.backgroundImage="url(../ASSET/Images/stars/"+images[indval]+")";
+            }
+        }
+        function openImg(pic,name,info)
+        {
+            document.getElementById("subWindow").removeAttribute("style")
+            document.getElementById("subWindoImg").src='../ASSET/Images/stars/'+pic+".jpg"
+            document.getElementById("imageHeading").innerHTML=name;
+            document.getElementById("imgpara").innerHTML=info;
+        }
+        function closeImg()
+        {
+            document.getElementById("subWindow").setAttribute("style","display:none")
+        }
---
+++ JAVASCRIPT/logout.js
@@ -0,0 +1,10 @@
+
+var number=99663994342
+function monkey()
+{
+    if(number==9963994392)
+    {
+        document.getElementById("input").setAttribute("style","display:none")
+    }
+
+}
